1be85f347deea673c0729eb7aeaf6f5b6889b290
Gonzalo Giulio <gggiulio77@gmail.com>
2023-10-25 21:11:09 -0300

    feat: add KeyboardButton enum

--- src/keyboard.rs
+++ src/keyboard.rs
@@ -1,4 +1,13 @@
 use serde::{Deserialize, Serialize};
 
+use crate::driver::keyboard::EnigoKey;
+
+// TODO: this brings EnigoKey, is not good but the Key enum es HUGE, maybe we can made it generic or some other shit
+// TODO: or maybe we can move all enigo shit related to /driver and use a feature flag
 #[derive(Debug, Clone, Deserialize, Serialize, Eq, PartialEq)]
-pub struct KeyboardButton;
+#[serde(rename_all = "snake_case")]
+pub enum KeyboardButton {
+    Sequence(String),
+    SequenceDsl(String),
+    Key(EnigoKey),
+}
