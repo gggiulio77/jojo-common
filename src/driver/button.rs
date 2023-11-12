e6440819e4d4dc00e07e70f62194e25eaa8100bb
Gonzalo Giulio <gggiulio77@gmail.com>
2023-11-11 21:01:54 -0300

    refactor: remove internal function

--- src/driver/button.rs
+++ src/driver/button.rs
@@ -2,8 +2,6 @@ use crate::keyboard::Key;
 use crate::{driver, mouse};
 use enigo::Enigo;
 
-use super::mouse::EnigoMouseButton;
-
 pub struct ButtonDriver {
     mouse_driver: Box<dyn driver::mouse::MouseAdapter + Send + Sync>,
     keyboard_driver: Box<dyn driver::keyboard::KeyboardAdapter + Send + Sync>,
@@ -18,7 +16,7 @@ impl Default for ButtonDriver {
     }
 }
 
-impl<'a> ButtonDriver {
+impl ButtonDriver {
     pub fn new(
         mouse_driver: Box<dyn driver::mouse::MouseAdapter + Send + Sync>,
         keyboard_driver: Box<dyn driver::keyboard::KeyboardAdapter + Send + Sync>,
@@ -29,29 +27,14 @@ impl<'a> ButtonDriver {
         }
     }
 
-    fn decode_mouse_button(
-        &self,
-        mouse_button: &'a mouse::MouseButton,
-    ) -> (EnigoMouseButton, &'a mouse::MouseButtonState) {
-        match mouse_button {
-            mouse::MouseButton::Left(state) => (EnigoMouseButton::Left, state),
-            mouse::MouseButton::Right(state) => (EnigoMouseButton::Right, state),
-            mouse::MouseButton::Back(state) => (EnigoMouseButton::Back, state),
-            mouse::MouseButton::Forward(state) => (EnigoMouseButton::Forward, state),
-            mouse::MouseButton::Middle(state) => (EnigoMouseButton::Middle, state),
-            mouse::MouseButton::ScrollDown(state) => (EnigoMouseButton::ScrollDown, state),
-            mouse::MouseButton::ScrollLeft(state) => (EnigoMouseButton::ScrollLeft, state),
-            mouse::MouseButton::ScrollRight(state) => (EnigoMouseButton::ScrollRight, state),
-            mouse::MouseButton::ScrollUp(state) => (EnigoMouseButton::ScrollUp, state),
-        }
-    }
-
-    // TODO: think a better way to do this, maybe a impl into o something similar
-    pub fn mouse_button_to_state(&mut self, mouse_button: &mouse::MouseButton) {
-        let (button, state) = self.decode_mouse_button(mouse_button);
+    pub fn mouse_button_to_state(
+        &mut self,
+        mouse_button: mouse::MouseButton,
+        state: mouse::MouseButtonState,
+    ) {
         match state {
-            mouse::MouseButtonState::Up => self.mouse_driver.mouse_move_up(button),
-            mouse::MouseButtonState::Down => self.mouse_driver.mouse_move_down(button),
+            mouse::MouseButtonState::Up => self.mouse_driver.mouse_move_up(mouse_button),
+            mouse::MouseButtonState::Down => self.mouse_driver.mouse_move_down(mouse_button),
         }
     }
 
@@ -64,5 +47,26 @@ impl<'a> ButtonDriver {
     }
     pub fn key_click(&mut self, key: Key) {
         self.keyboard_driver.key_click(key)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use crate::keyboard;
+
+    #[test]
+    fn mouse_test() {
+        let mut driver = ButtonDriver::default();
+
+        driver.mouse_button_to_state(mouse::MouseButton::Left, mouse::MouseButtonState::Down);
+        driver.mouse_button_to_state(mouse::MouseButton::Left, mouse::MouseButtonState::Up);
+    }
+
+    #[test]
+    fn keyboard_test() {
+        let mut driver = ButtonDriver::default();
+
+        driver.key_click(keyboard::Key::A);
     }
 }
